3c6e44bd26c24aabe9c9e23e41ff360ee767c725
Sijan Malla <sijan.malla101@gmail.com>
2023-09-16 19:29:04 -0500

    Added Reducer and Actions. Implemented Clear Cart and Remove Item.

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,39 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
+import reducer from './reducer';
+import cartItems from './data';
+
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions';
 
 const AppContext = createContext();
 
+const initialState = {
+  loading: false,
+  cart: new Map(cartItems.map((item) => [item.id, item])),
+};
+
 export const ContextProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE, payload: { id } });
+  };
+
+  return (
+    <AppContext.Provider value={{ ...state, clearCart, removeItem }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 export const useGlobalContext = () => useContext(AppContext);
 
---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions';
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: new Map() };
+  }
+  if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+    return { ...state, cart: newCart };
+  }
+  throw new Error(`no matching action type: ${action}`);
+};
+
+export default reducer;
